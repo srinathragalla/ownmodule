41f3c8fecbf90df2f5daa8e27d37ccf7811a25be
srinath ragalla <srinathragalla17@gmail.com>
2022-01-06 00:05:12 +0530

    own module

---
+++ ownmdul.js
@@ -0,0 +1,6 @@
+var calculator = require('./calcus.js');
+var x=9 , y=4;
+console.log("Addition of x and y is" + calculator.add(x,y));
+console.log("Subtraction of x and y is" + calculator.sub(x,y));
+console.log("Multiplication of x and y is" + calculator.mul(x,y));
+console.log("Division of x and y is" + calculator.div(x,y));
